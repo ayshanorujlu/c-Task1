134c2d222b17544b1a2695195659924f71dfcc32
user <user@WIN-2FA9I1VU4LK>
2022-12-26 02:08:36 +0400

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,52 @@
+﻿double Adding(double x,double y)
+{
+    return x + y;
+}
+
+double Subtract (double x,double y)
+{
+    return x - y;
+}
+
+double Multiply (double x, double y)
+{
+    return x * y;
+}
+
+double Divide(double x,double y)
+{
+    if (y == 0)
+        return 0;
+    else
+    return x / y;
+}
+
+Console.WriteLine("Enter x:");
+double.TryParse(Console.ReadLine(), out double number1);
+Console.WriteLine("Enter y:");
+Double.TryParse(Console.ReadLine(), out double number2);
+Console.WriteLine("Choose one of them:[+,-,*,/]");
+string operation=Console.ReadLine();
+
+
+switch (operation)
+{
+    case "+":
+        Console.WriteLine("Result:" + Adding(number1, number2));
+        break;
+    case "-":
+        Console.WriteLine("Result:" +Subtract(number1, number2));
+        break;
+    case "*":
+        Console.WriteLine("Result:" + Multiply(number1, number2));
+        break; 
+    case "/":
+        if (Divide(number1, number2) != 0)
+          Console.WriteLine("Result:" + Divide(number1, number2));
+        else
+            Console.WriteLine("Zero division error");
+        break;
+    default:
+        Console.WriteLine("Wrong input");
+        break;
+}
